fe46279e0ecf80132fe436cc8d2cb36a4d9076d7
huangfuhui <huangfuhui@meimeifa.com>
2018-08-18 10:33:15 +0800

    Initial Commit

---
+++ bitsync/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bitsync
+//
+//  Created by 黄富辉 on 2018/8/18.
+//  Copyright © 2018年 huangfuhui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
